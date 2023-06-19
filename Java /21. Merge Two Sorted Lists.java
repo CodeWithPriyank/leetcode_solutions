de8bfc17daf39ff54df26186264cd20a366b7d52
Priyank Goswami <109909710+CodeWithPriyank@users.noreply.github.com>
2023-06-19 23:49:55 +0530

    Created java file

--- Java /21. Merge Two Sorted Lists.java
+++ Java /21. Merge Two Sorted Lists.java
@@ -4,7 +4,7 @@
  *     int val;
  *     ListNode next;
  *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val)  { this.val = val; }
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
