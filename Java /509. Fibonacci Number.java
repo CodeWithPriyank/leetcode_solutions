b18e8bad6b1d798e8740337cc99789780e1e8b74
Priyank Goswami <109909710+CodeWithPriyank@users.noreply.github.com>
2023-07-03 03:18:20 +0530

    Create java file

---
+++ Java /509. Fibonacci Number.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int fib(int n) {
+        if(n == 0 || n == 1){
+            return n;
+        }
+
+        int a = 0;
+        int b = 1;
+        int ans = 0;
+
+        for(int i = 1; i < n; i++){
+            ans = a + b;
+            a = b;
+            b = ans;
+        }
+        return ans;
+    }
+}
